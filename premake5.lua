c868357df39d2e8363406aad8e8c367291ba52c7
Tianpei Luo <tluo4@stevens.edu>
2024-04-28 19:04:55 -0700

    Add premake file for generating project

---
+++ premake5.lua
@@ -0,0 +1,103 @@
+workspace "Tesla"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Tesla"
+    location "Tesla"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "Tesla/vendor/spdlog/include"
+    }
+
+    filter "system:windows" 
+        cppdialect "C++17"
+        staticruntime "on"
+        systemversion "10.0.22621.0"
+
+    defines {
+        "TL_PLATFORM_WINDOWS",
+        "TL_BUILD_DLL"
+    }
+
+    postbuildcommands
+    {
+        ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+    }
+
+    filter "configurations:Debug"
+        defines "TL_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+    defines "TL_RELEASE"
+    symbols "On"
+        
+    filter "configurations:Dist"
+    defines "TL_DIST"
+    symbols "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "Tesla/vendor/spdlog/include",
+        "Tesla/src"
+    }
+
+    links
+    {
+        "Tesla"
+    }
+
+    filter "system:windows" 
+        cppdialect "C++17"
+        staticruntime "on"
+        systemversion "10.0.22621.0"
+
+    defines {
+        "TL_PLATFORM_WINDOWS",
+    }
+
+    filter "configurations:Debug"
+        defines "TL_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "TL_RELEASE"
+        symbols "On"
+        
+    filter "configurations:Dist"
+        defines "TL_DIST"
+        symbols "On"
