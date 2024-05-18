fcd9236686c4e82f531f21ff8902e3211d4c7652
Tianpei Luo <tluo4@stevens.edu>
2024-05-17 19:38:39 -0700

    Add triangle but drawing using line Primitive

--- Tesla/src/Tesla/Application.h
+++ Tesla/src/Tesla/Application.h
@@ -32,6 +32,8 @@ namespace Tesla {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 		static Application* s_Instance;
+
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
 	};
 
 	Application* CreateApplication();
