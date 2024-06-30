21963288172af4e6d3070163021e40e4d089affe
teslasama <teslaluo@gmail.com>
2024-06-30 11:38:59 -0700

    Baisc ref-counting system to terminate glfw

--- Tesla/src/Tesla/Renderer/Buffer.h
+++ Tesla/src/Tesla/Renderer/Buffer.h
@@ -36,7 +36,7 @@ namespace Tesla {
 		uint32_t Offset;
 		bool Normalized;
 
-		BufferElement() {}
+		BufferElement() = default;
 
 		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
 			: Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
