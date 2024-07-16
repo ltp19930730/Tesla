3010e3ed0f166e9463e52f4470649f10eb5482d1
teslasama <teslaluo@gmail.com>
2024-07-16 00:12:10 -0700

    Add log file path

--- Tesla/src/Tesla/Core/Log.cpp
+++ Tesla/src/Tesla/Core/Log.cpp
@@ -1,6 +1,8 @@
 #include "tlpch.h"
 #include "Tesla/Core/Log.h"
 
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 
 namespace Tesla {
@@ -9,11 +11,21 @@ namespace Tesla {
 	std::shared_ptr<spdlog::logger> Log::s_clientLogger;
 
 	void Log::init() {
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_coreLogger = spdlog::stdout_color_mt("TESLA");
-		s_coreLogger->set_level(spdlog::level::trace);
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Tesla.log", true));
 
-		s_clientLogger = spdlog::stdout_color_mt("APP");
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		s_coreLogger = std::make_shared<spdlog::logger>("TESLA", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_coreLogger);
+		s_coreLogger->set_level(spdlog::level::trace);
+		s_coreLogger->flush_on(spdlog::level::trace);
+
+		s_clientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_clientLogger);
 		s_clientLogger->set_level(spdlog::level::trace);
+		s_clientLogger->flush_on(spdlog::level::trace);
 	}
 }
