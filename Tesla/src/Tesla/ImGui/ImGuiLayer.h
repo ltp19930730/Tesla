b9733f5bff6a8a8d476fd6ea86f089be9a42f94a
teslasama <teslaluo@gmail.com>
2024-09-26 23:35:25 -0700

    Add more style and theme

--- Tesla/src/Tesla/ImGui/ImGuiLayer.h
+++ Tesla/src/Tesla/ImGui/ImGuiLayer.h
@@ -19,6 +19,7 @@ namespace Tesla {
 		void Begin();
 		void End();
 		void BlockEvents(bool block) { m_BlockEvents = block; }
+		void SetDarkThemeColors();
 	private:
 		bool m_BlockEvents = true;
 		float m_Time = 0.0f;
